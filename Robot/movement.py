66b2683e041db80cf743d6853575a9952a0c7dab
Dvagoes <43369743+Dvagoes@users.noreply.github.com>
2022-01-24 08:39:26 +0000

    initial commit

---
+++ Robot/movement.py
@@ -0,0 +1,10 @@
+import Rpi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_0 = 4
+GPIO_1 = 17
+GPIO_2 = 27
+GPIO_3 = 22
+
---
+++ Robot/sensing.py
@@ -0,0 +1,42 @@
+#libraries
+import RPi.GPIO as GPIO
+import time
+
+
+
+try:
+    #GPIO setup
+    GPIO.setmode(GPIO.BCM)
+
+    #set pins
+    GPIO_TRIG = 5
+    GPIO_ECHO = 6
+
+    GPIO.setup(GPIO_TRIG, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+    
+    #activate Trigger
+    GPIO.output(GPIO_TRIG, True)
+    
+    #end pulse after 0.01ms
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIG, False)
+    
+    #save ToD
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+    
+    #save ToA
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+    
+    #calculate time difference
+    TimeElapsed = StopTime-StartTime
+    #use half of speed of sonic to calculate distance
+    distance = TimeElapsed * 17150
+    
+    print ("Distance Measured = %.1f cm" %distance)
+    
+finally:
+    GPIO.cleanup()
+    

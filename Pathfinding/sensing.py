efa8dd8da3695bdfe36c49f519573e9ae5ed2fea
Jorg <g.sheltoneckstein@gmail.com>
2022-04-26 01:45:30 +0100

    addded back files missed in previous commit

---
+++ Pathfinding/sensing.py
@@ -0,0 +1,44 @@
+#libraries
+import RPi.GPIO as GPIO
+import time
+
+def get_distance():
+    try:
+        print("using rangefinder...")
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+
+        #set pins
+        GPIO_TRIG = 5
+        GPIO_ECHO = 6
+
+        GPIO.setup(GPIO_TRIG, GPIO.OUT)
+        GPIO.setup(GPIO_ECHO, GPIO.IN)
+        
+        # activate Trigger
+        GPIO.output(GPIO_TRIG, True)
+        
+        # end pulse after 0.01ms
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIG, False)
+        
+        print ("waiting")
+        # save ToD
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
+        print (StartTime)
+        # save ToA
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+        print (StopTime)
+        # calculate time difference
+        TimeElapsed = StopTime-StartTime
+        # use half of speed of sonic to calculate distance
+        distance = TimeElapsed * 17150
+        
+        print ("Distance Measured = %.1f cm" %distance)
+        
+        
+    finally:
+        return distance
+    
